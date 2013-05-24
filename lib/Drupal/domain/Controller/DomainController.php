ed8dc4bf9ffdfca03f262437a2427bea40eab724
Ken Rickard <agentrickard@gmail.com>
2013-05-24 16:01:18 -0700

    Starts the converstion to the new routing system.

---
+++ lib/Drupal/domain/Controller/DomainController.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\domain\Controller\DomainController.
+ */
+
+namespace Drupal\domain\Controller;
+
+use Drupal\Core\ControllerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Controller routines for domain routes.
+ */
+class DomainController implements ControllerInterface {
+
+  /**
+   * Domain Manager Service.
+   *
+   * @var \Drupal\domain\DomainManager
+   */
+  protected $domainManager;
+
+   /**
+   * Injects DomainManager Service.
+   */
+  public static function create(ContainerInterface $container) {
+    return new static($container->get('domain.manager'));
+  }
+
+  /**
+   * Constructs a DomainController object.
+   */
+  public function __construct(DomainManager $domainManager) {
+    $this->domainManager = $domainManager;
+  }
+
+  /**
+   * Returns the admin form.
+   */
+  public function adminOverview() {
+    $domains = domain_load_multiple();
+    module_load_include('inc', 'domain', 'domain.admin');
+    return drupal_get_form('domain_overview_form', $domains);
+  }
+
+}
