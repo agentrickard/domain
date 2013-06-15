f0c03c5ad151bce5a3bc1b37f5b85cd28c228b21
Ken Rickard <agentrickard@gmail.com>
2013-06-15 16:39:11 -0400

    Fixes ControllerInterface directive.

--- lib/Drupal/domain/Controller/DomainController.php
+++ lib/Drupal/domain/Controller/DomainController.php
@@ -6,7 +6,7 @@
 
 namespace Drupal\domain\Controller;
 
-use Drupal\Core\ControllerInterface;
+use Drupal\Core\Controller\ControllerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\domain\DomainManager;
 
