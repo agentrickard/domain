4e1631d37b78031f2effaef359e0ba13c8b63c96
Ken Rickard <agentrickard@gmail.com>
2013-06-28 17:03:44 +0100

    Adds the Domain Switcher block.

---
+++ lib/Drupal/domain/Plugin/Block/DomainSwitcherBlock.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\domain\Plugin\Block\DomainSwitcherBlock.
+ */
+
+namespace Drupal\domain\Plugin\Block;
+
+use Drupal\block\BlockBase;
+use Drupal\Component\Annotation\Plugin;
+use Drupal\Core\Annotation\Translation;
+
+/**
+ * Provides a block that links to all domains.
+ *
+ * @Plugin(
+ *   id = "domain_switcher_block",
+ *   admin_label = @Translation("Domain switcher"),
+ *   module = "domain"
+ * )
+ */
+class DomainSwitcherBlock extends BlockBase {
+
+  /**
+   * Overrides \Drupal\block\BlockBase::access().
+   */
+  public function access() {
+    return user_access('administer domains');
+  }
+
+  /**
+   * Build the output.
+   *
+   * @TODO: abstract or theme this function?
+   */
+  public function build() {
+    $active_domain = domain_get_domain();
+    $items = array();
+    foreach (domain_load_multiple() as $domain) {
+      $string = l($domain->name, $domain->url, array('external' => TRUE));
+      if (!$domain->status) {
+        $string .= '*';
+      }
+      if ($domain->machine_name == $active_domain->machine_name) {
+        $string = '<em>' . $string . '</em>';
+      }
+
+      $items[] = $string;
+    }
+    return array(
+      '#theme' => 'item_list',
+      '#items' => $items,
+    );
+  }
+
+}
