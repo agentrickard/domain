ae879911528532c745dea32c43bbd381a61beef2
Ken Rickard <agentrickard@gmail.com>
2013-07-11 18:33:11 -0400

    Fixes ajax controller.

--- lib/Drupal/domain/Controller/DomainController.php
+++ lib/Drupal/domain/Controller/DomainController.php
@@ -56,47 +56,59 @@ class DomainController implements ControllerInterface {
     return entity_get_form($domain);
   }
 
+  /**
+   * Controller for handling Ajax operations from the overview page.
+   *
+   * @param \Drupal\domain\DomainInterface
+   *   A domain record object.
+   * @param $op
+   *   The operation being performed.
+   */
   public function ajaxOperation(DomainInterface $domain, $op = NULL) {
-  // @todo CSRF tokens are validated in page callbacks rather than access
-  //   callbacks, because access callbacks are also invoked during menu link
-  //   generation. Add token support to routing: http://drupal.org/node/755584.
-  $token = drupal_container()->get('request')->query->get('token');
-  $allowed_actions = array('enable', 'disable', 'default');
+    // @todo CSRF tokens are validated in page callbacks rather than access
+    //   callbacks, because access callbacks are also invoked during menu link
+    //   generation. Add token support to routing: http://drupal.org/node/755584.
+    $token = drupal_container()->get('request')->query->get('token');
+    $allowed_actions = array('enable', 'disable', 'default');
 
-  if (!in_array($op, $allowed_actions) || !isset($token) || !drupal_valid_token($token)) {
-    throw new AccessDeniedHttpException();
-  }
+    if (!in_array($op, $allowed_actions) || !isset($token) || !drupal_valid_token($token)) {
+      throw new AccessDeniedHttpException();
+    }
 
-  $success = FALSE;
-  switch($op) {
-    case 'default':
-      $domain->saveDefault();
-      $verb = t('set as default');
-      if ($domain->isDefault()) {
-        $success = TRUE;
-      }
-      break;
-    case 'enable':
-      $domain->enable();
-      $verb = t('has been enabled.');
-      if ($domain->status) {
-        $success = TRUE;
-      }
-      break;
-    case 'disable':
-      $domain->disable();
-      $verb = t('has been disabled.');
-      if (!$domain->status) {
-        $success = TRUE;
-      }
-      break;
-  }
-  if ($success) {
-    drupal_set_message(t('Domain record !verb.', array('!verb' => $verb)));
-  }
-  // Return to the invoking page.
-  $destination = drupal_get_destination();
-  return new RedirectResponse($destination['destination']);
+    $success = FALSE;
+    switch($op) {
+      case 'default':
+        $domain->saveDefault();
+        $verb = t('set as default');
+        if ($domain->isDefault()) {
+          $success = TRUE;
+        }
+        break;
+      case 'enable':
+        $domain->enable();
+        $verb = t('has been enabled.');
+        if ($domain->status) {
+          $success = TRUE;
+        }
+        break;
+      case 'disable':
+        $domain->disable();
+        $verb = t('has been disabled.');
+        if (!$domain->status) {
+          $success = TRUE;
+        }
+        break;
+    }
+
+    // @TODO: This no longer works in Drupal 8.
+    // See https://drupal.org/node/2023445#comment-7638261
+    if ($success) {
+      drupal_set_message(t('Domain record !verb.', array('!verb' => $verb)));
+    }
+
+    // Return to the invoking page.
+    $destination = drupal_get_destination();
+    return new RedirectResponse($destination['destination']);
   }
 
 }
