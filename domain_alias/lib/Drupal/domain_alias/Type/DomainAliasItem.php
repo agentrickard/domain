872c8aa020280197be68e8f0bc64f66eac60a1de
Ken Rickard <agentrickard@gmail.com>
2013-07-13 12:52:03 -0400

    Removes redundant domain_id storage.

--- domain_alias/lib/Drupal/domain_alias/Type/DomainAliasItem.php
+++ domain_alias/lib/Drupal/domain_alias/Type/DomainAliasItem.php
@@ -21,4 +21,52 @@ class DomainAliasItem extends FieldItemBase {
    */
   static $propertyDefinitions;
 
+  /**
+   * Implements ComplexDataInterface::getPropertyDefinitions().
+   */
+  public function getPropertyDefinitions() {
+    if (!isset(static::$propertyDefinitions)) {
+      static::$propertyDefinitions['pattern'] = array(
+        'type' => 'string',
+        'constraints' => array(
+          'EntityType' => 'domain',
+        ),
+        'label' => t('Pattern'),
+        'description' => t('The referenced domain record'),
+      );
+      static::$propertyDefinitions['redirect'] = array(
+        'type' => 'integer',
+        'label' => t('Redirect'),
+      );
+    }
+    return static::$propertyDefinitions;
+  }
+
+  /**
+   * Overrides \Drupal\Core\Entity\Field\FieldItemBase::setValue().
+   */
+  public function setValue($values) {
+    // Treat the values as property value of the entity field, if no array
+    // is given.
+    if (!is_array($values)) {
+      $values = array('entity' => $values);
+    }
+
+    // Entity is computed out of the ID, so we only need to update the ID. Only
+    // set the entity field if no ID is given.
+    if (isset($values['domain_id'])) {
+      $this->properties['domain_id']->setValue($values['domain_id']);
+    }
+    elseif (isset($values['entity'])) {
+      $this->properties['entity']->setValue($values['entity']);
+    }
+    else {
+      $this->properties['entity']->setValue(NULL);
+    }
+    unset($values['entity'], $values['domain_id']);
+    if ($values) {
+      throw new \InvalidArgumentException('Property ' . key($values) . ' is unknown.');
+    }
+  }
+
 }
