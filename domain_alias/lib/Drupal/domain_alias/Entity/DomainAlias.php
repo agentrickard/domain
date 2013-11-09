284f8a44842b31010ce7236c98f723d63d7ee8c8
Ken Rickard <agentrickard@gmail.com>
2013-11-09 17:42:52 -0500

    Base DomainAlias class definition.

---
+++ domain_alias/lib/Drupal/domain_alias/Entity/DomainAlias.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\domain_alias\Entity\DomainAlias.
+ */
+
+namespace Drupal\domain_alias\Entity;
+
+use Drupal\domain_alias\DomainAliasInterface;
+
+use Drupal\Core\Config\Entity\ConfigEntityBase;
+use Drupal\Core\Entity\EntityStorageControllerInterface;
+
+/**
+ * Defines the domain alias entity.
+ *
+ * @EntityType(
+ *   id = "domain_alias",
+ *   label = @Translation("Domain alias"),
+ *   module = "domain_alias",
+ *   controllers = {
+ *     "storage" = "Drupal\Core\Config\Entity\ConfigStorageController",
+ *     "access" = "Drupal\domain\DomainAccessController",
+ *     "list" = "Drupal\domain_alias\DomainAliasListController",
+ *     "form" = {
+ *       "default" = "Drupal\domain_alias\DomainAliasFormController",
+ *       "edit" = "Drupal\domain_alias\DomainAliasFormController",
+ *       "delete" = "Drupal\domain_alias\DomainAliasFormController"
+ *     }
+ *   },
+ *   config_prefix = "domain_alias.alias",
+ *   admin_permission = "administer domains",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "domain_uuid" = "domain_uuid",
+ *     "uuid" = "uuid",
+ *   },
+ * )
+ */
+class DomainAlias extends ConfigEntityBase implements DomainAliasInterface {
+
+  /**
+   * The ID of the domain entity.
+   *
+   * @var string
+   */
+  public $id;
+
+
+  /**
+   * Valiadates an alias.
+   */
+  public function validate() {
+    $pattern = $this->pattern;
+  }
+
+}
