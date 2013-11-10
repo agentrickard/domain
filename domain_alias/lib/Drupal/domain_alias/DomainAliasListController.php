fc1a1ba889e33f67047e3edde36814ba2b640fe5
Ken Rickard <agentrickard@gmail.com>
2013-11-10 12:31:05 -0500

    Menu structure for domain alias.

---
+++ domain_alias/lib/Drupal/domain_alias/DomainAliasListController.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\domain_alias\Form\DomainAliasListController.
+ */
+
+namespace Drupal\domain_alias;
+
+use Drupal\Core\Config\Entity\ConfigEntityListController;
+use Drupal\Core\Entity\EntityInterface;
+
+/**
+ * User interface for the domain alias overview screen.
+ */
+class DomainAliasListController extends ConfigEntityListController {
+
+}
