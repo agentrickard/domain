6f8f57a1402086ea3f107e778da798d0a72db0dc
Ken Rickard <agentrickard@gmail.com>
2013-11-10 18:18:36 -0500

    Validation for domain alias records.

--- domain_alias/lib/Drupal/domain_alias/Entity/DomainAlias.php
+++ domain_alias/lib/Drupal/domain_alias/Entity/DomainAlias.php
@@ -85,6 +85,39 @@ class DomainAlias extends ConfigEntityBase implements DomainAliasInterface {
    */
   public function validate() {
     $pattern = $this->pattern;
+
+    // 1) Check that the alias only has one wildcard.
+    $count = substr_count($pattern, '*') + substr_count($pattern, '?');
+    if ($count > 1) {
+      return t('You may only have one wildcard character in each alias.');
+    }
+    // 2) Only one colon allowed, and it must be followed by an integer.
+    $count = substr_count($pattern, ':');
+    if ($count > 1) {
+      return t('You may only have one colon ":" character in each alias.');
+    }
+    elseif ($count == 1) {
+      $int = substr($pattern, strpos($pattern, ':') + 1);
+      if (!is_numeric($int)) {
+        return t('A colon may only be followed by an integer indicating the proper port.');
+      }
+    }
+    // 3) Check that the alias doesn't contain any invalid characters.
+    $check = preg_match('/^[a-z0-9\.\+\-\*\?:]*$/', $pattern);
+    if ($check == 0) {
+      return t('The pattern contains invalid characters.');
+    }
+    // 4) Check that the alias is not a direct match for a registered domain.
+    $check = preg_match('/[a-z0-9\.\+\-:]*$/', $pattern);
+    if ($check == 1 && $test = domain_load_hostname($pattern)) {
+      return t('The pattern matches an existing domain record.');
+    }
+    // 5) Check that the alias is unique across all records.
+    if ($alias = domain_alias_pattern_load($pattern)) {
+      if ($alias->id() != $this->id()) {
+        return t('The pattern already exists.');
+      }
+    }
   }
 
 }
