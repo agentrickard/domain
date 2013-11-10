4af7b535738a2d2aee75b7c481b2ffa5ddd33261
Ken Rickard <agentrickard@gmail.com>
2013-11-10 12:32:39 -0500

    Adds form controller stub for domain alias.

---
+++ domain_alias/lib/Drupal/domain_alias/DomainAliasFormController.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\domain_alias\DomainAliasFormController.
+ */
+
+namespace Drupal\domain_alias;
+
+use Drupal\Core\Entity\EntityFormController;
+use Drupal\Core\Entity\EntityInterface;
+
+/**
+ * Base form controller for domain alias edit forms.
+ */
+class DomainAliasFormController extends EntityFormController {
+
+}
