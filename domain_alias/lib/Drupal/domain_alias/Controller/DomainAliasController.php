8c8975d912fc312f209bbad154a7a8efd5ca9e90
Ken Rickard <agentrickard@gmail.com>
2013-11-11 09:24:18 -0500

    Abstracts the DomainController to allow domains to be passed as arguments to other controllers.

--- domain_alias/lib/Drupal/domain_alias/Controller/DomainAliasController.php
+++ domain_alias/lib/Drupal/domain_alias/Controller/DomainAliasController.php
@@ -8,51 +8,12 @@
 namespace Drupal\domain_alias\Controller;
 
 use Drupal\domain\DomainInterface;
-use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
-use Drupal\Core\Entity\EntityManagerInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\domain\Controller\DomainControllerBase;
 
 /**
  * Returns responses for Domain Alias module routes.
  */
-class DomainAliasController implements ContainerInjectionInterface {
-
-  /**
-   * The entity storage.
-   *
-   * @var \Drupal\Core\Config\Entity\ConfigStorageController
-   */
-  protected $entityStorage;
-
-  /**
-   * The entity manager.
-   *
-   * @var \Drupal\Core\Entity\EntityManagerInterface
-   */
-  protected $entityManager;
-
-  /**
-   * Constructs a new DomainAliasController.
-   *
-   * @param \Drupal\Core\Config\Entity\ConfigStorageController $entity_storage
-   *   The storage controller.
-   * @param \Drupal\Core\Entity\EntityManagerInterface $entity_manager
-   *   The entity manager.
-   */
-  public function __construct(EntityStorageControllerInterface $entity_storage, EntityManagerInterface $entity_manager) {
-    $this->entityStorage = $entity_storage;
-    $this->entityManager = $entity_manager;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('entity.manager')->getStorageController('domain_alias'),
-      $container->get('entity.manager')
-    );
-  }
+class DomainAliasController extends DomainControllerBase {
 
   /**
    * Provides the domain alias submission form.
---
+++ lib/Drupal/domain/Controller/DomainControllerBase.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\domain\Controller\DomainControllerBase.
+ */
+
+namespace Drupal\domain\Controller;
+
+use Drupal\domain\DomainInterface;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Entity\EntityManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Sets a base class for injecting domain information into controllers.
+ *
+ * This class is useful in cases where your controller needs to respond to
+ * a domain argument. Drupal doesn't allow that natively, so we use this base
+ * class to allow router arguments to be passed a domain object.
+ *
+ * @see \Drupal\domain_alias\Controller\DomainAliasController
+ */
+class DomainControllerBase implements ContainerInjectionInterface {
+
+  /**
+   * The entity storage.
+   *
+   * @var \Drupal\Core\Config\Entity\ConfigStorageController
+   */
+  protected $entityStorage;
+
+  /**
+   * The entity manager.
+   *
+   * @var \Drupal\Core\Entity\EntityManagerInterface
+   */
+  protected $entityManager;
+
+  /**
+   * Constructs a new DomainAliasController.
+   *
+   * @param \Drupal\Core\Config\Entity\ConfigStorageController $entity_storage
+   *   The storage controller.
+   * @param \Drupal\Core\Entity\EntityManagerInterface $entity_manager
+   *   The entity manager.
+   */
+  public function __construct(EntityStorageControllerInterface $entity_storage, EntityManagerInterface $entity_manager) {
+    $this->entityStorage = $entity_storage;
+    $this->entityManager = $entity_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    // Here we return a simple manager; this may need to be overwritten for
+    // class implementations.
+    return new static(
+      $container->get('entity.manager')->getStorageController('domain'),
+      $container->get('entity.manager')
+    );
+  }
+
+}
