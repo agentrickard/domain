2c2163398f1cced29b4324a4560fb8ff03e50011
Ken Rickard <agentrickard@gmail.com>
2014-08-11 16:17:42 -0400

    Changes EntityAccessController reference to EntityAccessControlHandler.

--- domain/src/DomainAccessController.php
+++ domain/src/DomainAccessController.php
@@ -8,7 +8,7 @@
 namespace Drupal\domain;
 
 use Drupal\Core\Language\Language;
-use Drupal\Core\Entity\EntityAccessController;
+use Drupal\Core\Entity\EntityAccessControlHandler;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\Entity;
 use Drupal\Core\Session\AccountInterface;
@@ -16,7 +16,7 @@ use Drupal\Core\Session\AccountInterface;
 /**
  * Defines the access controller for the domain entity type.
  */
-class DomainAccessController extends EntityAccessController {
+class DomainAccessController extends EntityAccessControlHandler {
 
   /**
    * {@inheritdoc}
