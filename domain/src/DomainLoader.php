c7f7bcc133398b1ece450c9eb3d3d096a05af44f
Ken Rickard <agentrickard@gmail.com>
2014-09-30 11:45:06 +0200

    Creates the DomainL:oader service.

---
+++ domain/src/DomainLoader.php
@@ -0,0 +1,172 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\domain\DomainLoader.
+ */
+
+namespace Drupal\domain;
+
+use Drupal\domain\DomainLoaderInterface;
+use Drupal\domain\DomainInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Config\TypedConfigManagerInterface;
+
+class DomainLoader implements DomainLoaderInterface {
+
+  /**
+   * The module handler.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * The typed config handler.
+   *
+   * @var Drupal\Core\Config\TypedConfigManagerInterface
+   */
+  protected $typed_config;
+
+  /**
+   * Constructs a DomainResolver object.
+   *
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The module handler.
+   * @param Drupal\Core\Config\TypedConfigManagerInterface $typed_config
+   *   The typed config handler.
+   */
+  public function __construct(ModuleHandlerInterface $module_handler) {
+    $this->moduleHandler = $module_handler;
+    $this->typedConfig = $typed_config;
+  }
+
+  /**
+   * @return array
+   *   An array keyed by field name and containing the name and
+   *   label for the field.
+   */
+  public function getSchema() {
+    $fields = $this->typedConfig->getDefinition('domain.record.*');
+    return isset($fields['mapping']) ? $fields['mapping'] : array();
+  }
+
+  /**
+   * Returns the id of the default domain.
+   *
+   * @return
+   *   The id of the default domain or FALSE if none is set.
+   */
+  public function getDefaultId() {
+    // manager getDefaultId.
+    $result = entity_load_multiple_by_properties('domain', array('is_default' => TRUE));
+    if (!empty($result)) {
+      return key($result);
+    }
+    return FALSE;
+  }
+
+  /**
+   * Gets the default domain object.
+   */
+  public function getDefaultDomain() {
+    $result = entity_load_multiple_by_properties('domain', array('is_default' => TRUE));
+    if (!empty($result)) {
+      return current($result);
+    }
+    return FALSE;
+  }
+
+  /**
+   * Loads multiple domains.
+   */
+  public function loadMultiple($ids = array(), $reset = FALSE) {
+    return entity_load_multiple('domain', $ids, $reset);
+  }
+
+  /**
+   * Loads multiple domains and sorts by weight.
+   */
+  public function loadMultipleSorted($ids = array()) {
+    $domains = $this->loadMultiple();
+    return uasort($domains, array($this, 'sort'));
+  }
+
+  /**
+   * Loads a domain record by hostname lookup.
+   */
+  public function loadByHostname($hostname) {
+    $entities = entity_load_multiple_by_properties('domain', array('hostname' => $hostname));
+    if (empty($entities)) {
+      return FALSE;
+    }
+    return current($entities);
+  }
+
+  /**
+   * Creates a new domain record object.
+   */
+  public function createDomain($inherit = FALSE, array $values = array()) { }
+
+  /**
+   * Gets the next numeric id for a domain.
+   */
+  public function getNextId() {
+    $domains = $this->loadMultiple();
+    $max = 0;
+    foreach ($domains as $domain) {
+      if ($domain->domain_id > $max) {
+        $max = $domain->domain_id;
+      }
+    }
+    return $max + 1;
+  }
+
+  /**
+   * Gets the hostname of the active request.
+   */
+  public function getHostname() {
+    return !empty($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'localhost';
+  }
+
+  /**
+   * Gets the machine name of a host, used as primary key.
+   */
+  public function getMachineName($hostname) {
+    return preg_replace('/[^a-z0-9_]+/', '_', $hostname);
+  }
+
+  /**
+   * Gets the id of the active domain.
+   */
+  public function getActiveId() {
+    return $this->getActiveDomain()->id();
+  }
+
+  /**
+   * Returns the list of domains formatted for a form options list.
+   */
+  public function optionsList() {
+    $list = array();
+    foreach ($this->loadMultipleSorted() as $id => $domain) {
+      $list[$id] = $domain->name;
+    }
+    return $list;
+  }
+
+  /**
+   * Sorts domains by weight.
+   */
+  public function sort($a, $b) {
+    return $a->weight > $b->weight;
+  }
+
+  /**
+   * Gets the list of required fields.
+   */
+  public function getRequiredFields() {
+    return array('hostname', 'name', 'id', 'scheme', 'status', 'weight');
+  }
+
+
+}
