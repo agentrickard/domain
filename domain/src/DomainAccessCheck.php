185247f1f8606b394f0fd01e0be990e4b2719dda
Ken Rickard <agentrickard@gmail.com>
2014-09-30 17:18:42 +0200

    Basic access check for inactive domains.

--- domain/src/DomainAccessCheck.php
+++ domain/src/DomainAccessCheck.php
@@ -2,26 +2,20 @@
 
 /**
  * @file
- * Defines \Drupal\domain\src\DoaminAccessCheck.
+ * Defines \Drupal\domain\src\DomainAccessCheck.
  */
 
-namespace 'Drupal\domain';
+namespace Drupal\domain;
 
-use Drupal\Core\Access\AccessCheckInterface;
+use Drupal\Core\Routing\Access\AccessInterface;
 use Drupal\Core\Access\AccessResult;
-use Drupal\Core\Access\AccessManagerInterface;
 use Drupal\domain\DomainInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Symfony\Component\Routing\Route;
 
-class DomainAccessCheck {
-
-  /**
-   * The access manager.
-   *
-   * @var \Drupal\Core\Access\AccessManagerInterface
-   */
-  protected $accessManager;
+class DomainAccessCheck implements AccessInterface {
 
   /**
    * The current user.
@@ -31,20 +25,46 @@ class DomainAccessCheck {
   protected $account;
 
   /**
+   * @var \Drupal\domain\DomainResolverInterface
+   */
+  protected $domainResolver;
+
+  /**
    * The module handler.
    *
    * @var \Drupal\Core\Extension\ModuleHandlerInterface
    */
   protected $module_handler;
 
-  public function __construct(AccessManagerInterface $access_manager, AccountInterface $account, ModuleHandlerInterface $module_handler) {
-    $this->accessManager = $access_manager;
-    $this->moduleHandler = $module_handler;
+  public function __construct(DomainResolverInterface $resolver, AccountInterface $account, ModuleHandlerInterface $module_handler) {
+    $this->domainResolver = $resolver;
     $this->account = $account;
-    #$this->accessManager->addCheckService('domain.check', 'access', array(), TRUE);
+    $this->moduleHandler = $module_handler;
   }
 
-  public function access() {
-    dpm('foo');
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(RouteMatchInterface $route_match) {
+    return TRUE;
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function access(Route $route) {
+    $domain = $this->domainResolver->resolveActiveDomain();
+    // Is the domain allowed?
+    if (!$domain) {
+      return AccessResult::neutral();
+    }
+    if ($domain->isEnabled()) {
+      return AccessResult::allowed();
+    }
+    // @todo: how to issue a redirect from here.
+    else {
+      return AccessResult::allowedIfHasPermissions($this->account, array('administer domains', 'access inactive domains'), 'OR');
+    }
+  }
+
 }
