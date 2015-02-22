bbf8820ccc05945245f5bd6f2fe0d344a6048a17
agentrickard <agentrickard@gmail.com>
2015-02-22 10:43:02 -0500

    Domain Access records storage test.

--- domain_access/src/Tests/DomainAccessRecordsTest.php
+++ domain_access/src/Tests/DomainAccessRecordsTest.php
@@ -25,11 +25,34 @@ class DomainAccessRecordsTest extends DomainTestBase {
 
   function setUp() {
     parent::setUp();
-
     // Run the install hook.
     // @TODO: figure out why this is necessary.
     module_load_install('domain_access');
     domain_access_install();
+  }
+
+  /**
+   * Creates a node and tests the creation of node access rules.
+   */
+  function testDomainAccessRecords() {
+    // Create 5 domains.
+    $this->domainCreateTestDomains(5);
+    // Assign a node to a random domain.
+    $domains = domain_load_multiple();
+    $active_domain = array_rand($domains, 1);
+    $domain = $domains[$active_domain];
+    // Create an article node.
+    $node1 = $this->drupalCreateNode(array(
+      'type' => 'article',
+      DOMAIN_ACCESS_NODE_FIELD => array($domain->id()),
+    ));
+    $this->assertTrue(entity_load('node', $node1->id()), 'Article node created.');
+
+    // Check to see if grants added by domain_node_access_records made it in.
+    $records = db_query('SELECT realm, gid FROM {node_access} WHERE nid = :nid', array(':nid' => $node1->id()))->fetchAll();
+    $this->assertEqual(count($records), 1, 'Returned the correct number of rows.');
+    $this->assertEqual($records[0]->realm, 'domain_id', 'Grant with domain_id acquired for node.');
+    $this->assertEqual($records[0]->gid, $domain->getDomainId(), 'Grant with proper id acquired for node.');
   }
 
 }
