88c18d768bef75218026716efda06fc3823717e0
agentrickard <agentrickard@gmail.com>
2015-11-29 13:47:19 -0500

    Fixes failing test.

--- domain_access/src/Tests/DomainAccessEntityReferenceTest.php
+++ domain_access/src/Tests/DomainAccessEntityReferenceTest.php
@@ -58,7 +58,7 @@ class DomainAccessEntityReferenceTest extends DomainTestBase {
    * Tests the storage of the domain access field.
    */
   function testDomainAccessFieldStorage() {
-    $this->admin_user = $this->drupalCreateUser(array('bypass node access', 'administer content types', 'administer node fields', 'administer node display', 'administer domains'));
+    $this->admin_user = $this->drupalCreateUser(array('bypass node access', 'administer content types', 'administer node fields', 'administer node display', 'administer domains', 'publish to any domain'));
     $this->drupalLogin($this->admin_user);
 
     // Create 5 domains.
