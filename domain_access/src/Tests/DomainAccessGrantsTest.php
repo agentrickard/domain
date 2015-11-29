419f75244f53249c66256b21d9c5c676c065941d
agentrickard <agentrickard@gmail.com>
2015-11-29 11:04:49 -0500

    Proper test coverage for node grants.

--- domain_access/src/Tests/DomainAccessGrantsTest.php
+++ domain_access/src/Tests/DomainAccessGrantsTest.php
@@ -22,7 +22,7 @@ class DomainAccessGrantsTest extends DomainTestBase {
    *
    * @var array
    */
-  public static $modules = array('domain', 'domain_access', 'field', 'field_ui');
+  public static $modules = array('domain', 'domain_access', 'field', 'node');
 
   function setUp() {
     parent::setUp();
@@ -41,6 +41,8 @@ class DomainAccessGrantsTest extends DomainTestBase {
    * Creates a node and tests the creation of node access rules.
    */
   function testDomainAccessGrants() {
+    // The {node_access} table is not emptied properly by the setup.
+    db_delete('node_access')->execute();
     // Create 5 domains.
     $this->domainCreateTestDomains(5);
     // Assign a node to a random domain.
@@ -54,64 +56,34 @@ class DomainAccessGrantsTest extends DomainTestBase {
     ));
     $this->assertTrue(\Drupal::entityManager()->getStorage('node')->load($node1->id()), 'Article node created.');
 
-    // Test the response of the node on each site. Should allow access.
+    // Test the response of the node on each site. Should allow access only to
+    // the selected site.
     foreach ($domains as $domain) {
-      $this->drupalGet($domain->getPath() . 'node/' . $node1->id());
-      $this->assertRaw($node1->getTitle(), 'Loaded the proper domain.');
+      $path = $domain->getPath() . 'node/' . $node1->id();
+      $this->drupalGet($path);
+      if ($domain->id() == $active_domain) {
+        $this->assertResponse(200);
+        $this->assertRaw($node1->getTitle(), 'Article found on domain.');
+      }
+      else {
+        $this->assertResponse(403);
+      }
     }
-    // Ensures user without 'access content' permission can do nothing.
-    $web_user1 = $this->drupalCreateUser(array('create article content', 'edit any article content', 'delete any article content'));
-    $this->assertNodeAccess(array('view' => FALSE, 'update' => FALSE, 'delete' => FALSE), $node1, $web_user1);
-    // Grant access content and the user can view the node.
-    user_role_grant_permissions(DRUPAL_AUTHENTICATED_RID, array('access content'));
-    // We have to use a new user here because the access check is cached.
-    $web_user2 = $this->drupalCreateUser(array('access content', 'create article content'));
-    $this->assertNodeAccess(array('view' => TRUE, 'update' => FALSE, 'delete' => FALSE), $node1, $web_user2);
-    // Check global update and delete grants.
-    // We have to use a new user here because the access check is cached.
-    $web_user3 = $this->drupalCreateUser(array('access content', 'create article content', 'edit any article content', 'delete any article content'));
-    $this->addDomainToEntity('user', $web_user3->id(), $active_domain, DOMAIN_ACCESS_USER_FIELD);
-    user_role_grant_permissions(DRUPAL_AUTHENTICATED_RID, array('edit domain content', 'delete domain content'));
-    $this->assertNodeAccess(array('view' => TRUE, 'update' => TRUE, 'delete' => TRUE), $node1, $web_user3);
-  }
 
-  /**
-   * Asserts that node access correctly grants or denies access.
-   *
-   * @param array $ops
-   *   An associative array of the expected node access grants for the node
-   *   and account, with each key as the name of an operation (e.g. 'view',
-   *   'delete') and each value a Boolean indicating whether access to that
-   *   operation should be granted.
-   * @param \Drupal\node\Entity\Node $node
-   *   The node object to check.
-   * @param \Drupal\Core\Session\AccountInterface $account
-   *   The user account for which to check access.
-   */
-  function assertNodeAccess(array $ops, $node, AccountInterface $account) {
-    foreach ($ops as $op => $result) {
-      $this->assertEqual($result, $this->accessHandler->access($node, $op, $account), $this->nodeAccessAssertMessage($op, $result));
+    // Create an article node.
+    $node2 = $this->drupalCreateNode(array(
+      'type' => 'article',
+      DOMAIN_ACCESS_NODE_FIELD => array($domain->id()),
+      DOMAIN_ACCESS_ALL_FIELD => 1,
+    ));
+    $this->assertTrue(\Drupal::entityManager()->getStorage('node')->load($node2->id()), 'Article node created.');
+    // Test the response of the node on each site. Should allow access on all.
+    foreach ($domains as $domain) {
+      $path = $domain->getPath() . 'node/' . $node2->id();
+      $this->drupalGet($path);
+      $this->assertResponse(200);
+      $this->assertRaw($node2->getTitle(), 'Article found on domain.');
     }
-  }
-
-  /**
-   * Constructs an assert message for checking node access.
-   *
-   * @param string $operation
-   *   The operation to check access for.
-   * @param bool $result
-   *   Whether access should be granted or not.
-   *
-   * @return string
-   */
-  function nodeAccessAssertMessage($operation, $result) {
-    return format_string(
-      'Node access returns @result with operation %op.',
-      array(
-        '@result' => $result ? 'true' : 'false',
-        '%op' => $operation,
-      )
-    );
   }
 
 }
