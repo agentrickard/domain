4c3b11536428ba9edcdac93c75041f29b2c68282
agentrickard <agentrickard@gmail.com>
2016-01-16 16:17:17 -0500

    Cleans up the DomainValidator classes.

--- domain/src/DomainValidatorInterface.php
+++ domain/src/DomainValidatorInterface.php
@@ -16,11 +16,28 @@ interface DomainValidatorInterface {
 
   /**
    * Validates the hostname for a domain.
+   *
+   * @param Drupal\domain\DomainInterface $domain
+   *   A domain record.
+   *
+   * @return array
+   *   An array of validation errors. An empty array indicates a valid domain.
    */
   public function validate(DomainInterface $domain);
 
   /**
    * Tests that a domain responds correctly.
+   *
+   * This is a server-level configuration test. The core module provides an
+   * image file that we use to test the validity of domain-generated URLs.
+   *
+   * That file is /domain/tests/200.png.
+   *
+   * @param Drupal\domain\DomainInterface $domain
+   *   A domain record.
+   *
+   * @return integer
+   *   The server response code for the request.
    */
   public function checkResponse(DomainInterface $domain);
 
