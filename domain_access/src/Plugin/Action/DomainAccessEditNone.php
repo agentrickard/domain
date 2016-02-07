42d25998363de2aaf9a233601aff6c30dea68a23
agentrickard <agentrickard@gmail.com>
2016-02-07 17:13:35 -0500

    Rmove all affiliates from editors action.

---
+++ domain_access/src/Plugin/Action/DomainAccessEditNone.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\domain_access\Plugin\Action\DomainAccessEditNone.
+ */
+
+namespace Drupal\domain_access\Plugin\Action;
+
+use Drupal\Core\Action\ActionBase;
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ * Removes a user from all affiliates.
+ *
+ * @Action(
+ *   id = "domain_access_edit_none_action",
+ *   label = @Translation("Remove editors from all affiliates"),
+ *   type = "user"
+ * )
+ */
+class DomainAccessEditNone extends ActionBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute($entity = NULL) {
+    $entity->set(DOMAIN_ACCESS_ALL_FIELD, 0);
+    $entity->save();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
+    // @TODO: Check this logic.
+    /** @var \Drupal\user\UserInterface $object */
+    $access = $object->access('update', $account, TRUE)
+      ->andIf($object->roles->access('edit', $account, TRUE));
+
+    return $return_as_object ? $access : $access->isAllowed();
+  }
+
+}
