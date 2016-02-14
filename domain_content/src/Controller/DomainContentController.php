7f8a1c6551a97d1c7df61a37b75183d6e24f65cf
agentrickard <agentrickard@gmail.com>
2016-02-14 15:12:59 -0500

    Abstracts count functions.

--- domain_content/src/Controller/DomainContentController.php
+++ domain_content/src/Controller/DomainContentController.php
@@ -18,45 +18,53 @@ class DomainContentController extends DomainControllerBase {
 
   use StringTranslationTrait;
 
+  /**
+   * Generates a list of content by domain.
+   */
   public function contentList() {
     $build = [
       '#theme' => 'table',
       '#header' => [$this->t('Domain'), $this->t('Content count')],
     ];
-    $build['#rows'][] = [$this->l($this->t('All affiliates'), Url::fromUri('internal:/admin/content/domain-content/all_affiliates')), $this->getContentCount()];
+    $build['#rows'][] = [$this->l($this->t('All affiliates'), Url::fromUri('internal:/admin/content/domain-content/all_affiliates')), $this->getCount('node')];
     // @TODO: Inject this service.
     $domains = \Drupal::service('domain.loader')->loadMultipleSorted();
     foreach ($domains as $domain) {
-      $row = [$this->l($domain->label(), Url::fromUri('internal:/admin/content/domain-content/' . $domain->id())), $this->getContentCount($domain)];
+      $row = [$this->l($domain->label(), Url::fromUri('internal:/admin/content/domain-content/' . $domain->id())), $this->getCount('node', $domain)];
       $build['#rows'][] = $row;
     }
     return $build;
   }
 
+  /**
+   * Generates a list of editors by domain.
+   */
   public function editorsList() {
     $build = [
       '#theme' => 'table',
       '#header' => [$this->t('Domain'), $this->t('Editor count')],
     ];
-    $build['#rows'][] = [$this->l($this->t('All affiliates'), Url::fromUri('internal:/admin/content/domain-editors/all_affiliates')), $this->getEditorCount()];
+    $build['#rows'][] = [$this->l($this->t('All affiliates'), Url::fromUri('internal:/admin/content/domain-editors/all_affiliates')), $this->getCount('user')];
     // @TODO: Inject this service.
     $domains = \Drupal::service('domain.loader')->loadMultipleSorted();
     foreach ($domains as $domain) {
-      $row = [$this->l($domain->label(), Url::fromUri('internal:/admin/content/domain-editors/' . $domain->id())), $this->getEditorCount($domain)];
+      $row = [$this->l($domain->label(), Url::fromUri('internal:/admin/content/domain-editors/' . $domain->id())), $this->getCount('user', $domain)];
       $build['#rows'][] = $row;
     }
     return $build;
   }
 
-  public function contentPage(DomainInterface $domain) {
-    return ['#markup' => $domain->label()];
-  }
-
-  public function editorsPage(DomainInterface $domain) {
-    return ['#markup' => $domain->label()];
-  }
-
-  protected function getContentCount($domain = NULL, $entity_type = 'node') {
+  /**
+   * Counts the content for a domain.
+   *
+   * @param $entity_type
+   *  The entity type.
+   * @param DomainInterface $domain
+   *  The domain to query. If passed NULL, checks status for all affiliates.
+   *
+   * @return int
+   */
+  protected function getCount($entity_type = 'node', $domain = NULL) {
     if (is_null($domain)) {
       $field = DOMAIN_ACCESS_ALL_FIELD;
       $value = 1;
@@ -65,14 +73,11 @@ class DomainContentController extends DomainControllerBase {
       $field = DOMAIN_ACCESS_FIELD;
       $value = $domain->id();
     }
+    // Note that we ignore node access so these queries work on any domain.
     $query = \Drupal::entityQuery($entity_type)
       ->condition($field, $value);
 
     return count($query->execute());
-  }
-
-  protected function getEditorCount($domain = NULL, $entity_type = 'user') {
-    return $this->getContentCount($domain, $entity_type);
   }
 
 }
