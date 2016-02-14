ce609a3aec1b92dbef2030b10b6a9e2233486ada
agentrickard <agentrickard@gmail.com>
2016-02-14 14:54:40 -0500

    Fixes error in domain access field.

--- domain_access/src/Plugin/views/field/DomainAccessField.php
+++ domain_access/src/Plugin/views/field/DomainAccessField.php
@@ -21,10 +21,10 @@ class DomainAccessField extends Field {
    * {@inheritdoc}
    */
   public function getItems(ResultRow $values) {
+    $items = parent::getItems($values);
     // Override the default link generator, which wants to send us to the entity
-    // page, not the node we are looking at.
-    if ($this->options['settings']['link']) {
-      $items = parent::getItems($values);
+    // page, not the entity we are looking at.
+    if (!empty($this->options['settings']['link'])) {
       foreach ($items as &$item) {
         $object = $item['raw'];
         $entity = $object->getEntity();
