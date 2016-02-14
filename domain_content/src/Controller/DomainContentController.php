038d1840f815f05cb62516334480e7fb81902d8a
agentrickard <agentrickard@gmail.com>
2016-02-14 12:20:37 -0500

    Basic link handling in Domain Content.

--- domain_content/src/Controller/DomainContentController.php
+++ domain_content/src/Controller/DomainContentController.php
@@ -8,8 +8,8 @@ namespace Drupal\domain_content\Controller;
 
 use Drupal\domain\DomainInterface;
 use Drupal\domain\Controller\DomainControllerBase;
-use Drupal\Core\Routing\UrlGeneratorTrait;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Url;
 
 /**
  * Controller routines domain content pages.
@@ -17,29 +17,57 @@ use Drupal\Core\StringTranslation\StringTranslationTrait;
 class DomainContentController extends DomainControllerBase {
 
   use StringTranslationTrait;
-  use UrlGeneratorTrait;
 
   public function contentList() {
     $build = [
-      '#markup' => 'foo',
+      '#theme' => 'table',
+      '#header' => [$this->t('Domain'), $this->t('Content count')],
     ];
+    $build['#rows'][] = [$this->l($this->t('All affiliates'), Url::fromUri('internal:/admin/content/domain-content/all_affiliates')), $this->getEditorCount()];
+    // @TODO: Inject this service.
+    $domains = \Drupal::service('domain.loader')->loadMultipleSorted();
+    foreach ($domains as $domain) {
+      $row = [$this->l($domain->label(), Url::fromUri('internal:/admin/content/domain-content/' . $domain->id())), $this->getContentCount($domain)];
+      $build['#rows'][] = $row;
+    }
     return $build;
   }
 
   public function editorsList() {
     $build = [
-      '#markup' => 'bar',
+      '#theme' => 'table',
+      '#header' => [$this->t('Domain'), $this->t('Editor count')],
     ];
+    $build['#rows'][] = [$this->l($this->t('All affiliates'), Url::fromUri('internal:/admin/content/domain-editors/all_affiliates')), $this->getEditorCount()];
+    // @TODO: Inject this service.
+    $domains = \Drupal::service('domain.loader')->loadMultipleSorted();
+    foreach ($domains as $domain) {
+      $row = [$this->l($domain->label(), Url::fromUri('internal:/admin/content/domain-editors/' . $domain->id())), $this->getEditorCount($domain)];
+      $build['#rows'][] = $row;
+    }
     return $build;
   }
 
   public function contentPage(DomainInterface $domain) {
-
+    return ['#markup' => $domain->label()];
   }
 
   public function editorsPage(DomainInterface $domain) {
-
+    return ['#markup' => $domain->label()];
   }
 
+  protected function getContentCount($domain = NULL) {
+    if (is_null($domain)) {
+      return 50;
+    }
+    return 100;
+  }
+
+  protected function getEditorCount($domain = NULL) {
+    if (is_null($domain)) {
+      return 50;
+    }
+    return 100;
+  }
 
 }
