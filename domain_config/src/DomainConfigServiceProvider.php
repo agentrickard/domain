ef447ac22f2c97191e78835898b19736b2c4b102
bjorn <bjorn.axelsen@fagkommunikation.dk>
2016-03-31 14:01:59 +0300

    Improved class notation

--- domain_config/src/DomainConfigServiceProvider.php
+++ domain_config/src/DomainConfigServiceProvider.php
@@ -9,6 +9,7 @@ namespace Drupal\domain_config;
 
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\DependencyInjection\ServiceProviderBase;
+use Drupal\domain_config\Routing\DomainRouteProvider;
 
 /**
  * Overrides the router.route_provider service to point to our customized one.
@@ -20,6 +21,6 @@ class DomainConfigServiceProvider extends ServiceProviderBase {
    */
   public function alter(ContainerBuilder $container) {
     $definition = $container->getDefinition('router.route_provider');
-    $definition->setClass('Drupal\domain_config\Routing\DomainRouteProvider');
+    $definition->setClass(DomainRouteProvider::class);
   }
 }
