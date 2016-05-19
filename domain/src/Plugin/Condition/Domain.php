agentrickard/domain

commit 1ff4051622cecef2e4d56e091df75683bafc1549

Replace deprecated methods/classes/traits

domain/src/Plugin/Condition/Domain.php
@@ -65,7 +65,7 @@ class Domain extends ConditionPluginBase implements ContainerFactoryPluginInterf
       '#type' => 'checkboxes',
       '#title' => $this->t('When the following domains are active'),
       '#default_value' => $this->configuration['domains'],
-      '#options' => array_map('\Drupal\Component\Utility\SafeMarkup::checkPlain', \Drupal::service('domain.loader')->loadOptionsList()),
+      '#options' => array_map('\Drupal\Component\Utility\Html::checkPlain', \Drupal::service('domain.loader')->loadOptionsList()),
       '#description' => $this->t('If you select no domains, the condition will evaluate to TRUE for all requests.'),
       '#attached' => array(
         'library' => array(

domain/domain.drush.inc
@@ -5,9 +5,8 @@
  * Drush commands for Domain Access.
  */
 
+use Drupal\Component\Utility\Html;
 use Drupal\domain\DomainInterface;
-use Drupal\Core\Entity\EntityInterface;
-use Drupal\Component\Utility\SafeMarkup;
 
 /**
  * Implements hook_drush_command().
@@ -190,7 +189,7 @@ function drush_domain_list() {
   foreach ($domains as $domain) {
     $row = array();
     foreach ($header as $key => $name) {
-      $row[] = SafeMarkup::checkPlain($domain->get($key));
+      $row[] = Html::escape($domain->get($key));
     }
     $rows[] = $row;
   }

domain/src/Controller/DomainController.php
@@ -2,9 +2,10 @@
 
 namespace Drupal\domain\Controller;
 
+use Drupal\core\Url;
 use Drupal\domain\DomainInterface;
-use Drupal\Core\Routing\UrlGeneratorTrait;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 /**
  * Controller routines for AJAX callbacks for domain actions.
@@ -12,7 +13,6 @@ use Drupal\Core\StringTranslation\StringTranslationTrait;
 class DomainController {
 
   use StringTranslationTrait;
-  use UrlGeneratorTrait;
 
   /**
    * Handles AJAX operations from the overview form.
@@ -69,7 +69,8 @@ class DomainController {
     }
 
     // Return to the invoking page.
-    return $this->redirect('domain.admin');
+    $url = Url::fromRoute('domain.domain', array(), array('absolute' => TRUE));
+    return new RedirectResponse($url, 302);
   }
 
 }

domain/src/DomainViewBuilder.php
@@ -2,8 +2,8 @@
 
 namespace Drupal\domain;
 
+use Drupal\Component\Utility\Html;
 use Drupal\Core\Entity\EntityViewBuilder;
-use Drupal\Component\Utility\SafeMarkup;
 
 /**
  * Render controller for domain records.
@@ -31,8 +31,8 @@ class DomainViewBuilder extends EntityViewBuilder {
         if (!empty($entity->{$key}) && $display->getComponent($key)) {
           $class = str_replace('_', '-', $key);
           $entity->content[$key] = array(
-            '#markup' => SafeMarkup::checkPlain($entity->{$key}),
-            '#prefix' => '<div class="domain-' . $class . '">' . '<strong>' . SafeMarkup::checkPlain($key) . ':</strong><br />',
+            '#markup' => Html::escape($entity->{$key}),
+            '#prefix' => '<div class="domain-' . $class . '">' . '<strong>' . Html::escape($key) . ':</strong><br />',
             '#suffix' => '</div>',
           );
         }

domain/src/Plugin/Block/DomainServerBlock.php
@@ -2,7 +2,7 @@
 
 namespace Drupal\domain\Plugin\Block;
 
-use Drupal\Component\Utility\SafeMarkup;
+use Drupal\Component\Utility\Html;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Session\AccountInterface;
 
@@ -40,7 +40,7 @@ class DomainServerBlock extends DomainBlockBase {
     $header = array($this->t('Property'), $this->t('Value'));
     $rows[] = array(
       $this->t('HTTP_HOST request'),
-      SafeMarkup::checkPlain($_SERVER['HTTP_HOST']),
+      Html::escape($_SERVER['HTTP_HOST']),
     );
     // Check the response test.
     $domain->getResponse();
@@ -75,8 +75,8 @@ class DomainServerBlock extends DomainBlockBase {
         $value = empty($value) ? $this->t('FALSE') : $this->t('TRUE');
       }
       $rows[] = array(
-        SafeMarkup::checkPlain($key),
-        !is_array($value) ? SafeMarkup::checkPlain($value) : $this->printArray($value),
+        Html::escape($key),
+        !is_array($value) ? Html::escape($value) : $this->printArray($value),
       );
     }
     return array(
@@ -99,7 +99,7 @@ class DomainServerBlock extends DomainBlockBase {
     $items = array();
     foreach ($array as $key => $val) {
       if (!is_array($val)) {
-        $value = SafeMarkup::checkPlain($val);
+        $value = Html::escape($val);
       }
       else {
         $list = array();

domain/src/Tests/DomainActionsTest.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Drupal\domain\Tests;
+use Drupal\Component\Render\FormattableMarkup;
 
 /**
  * Tests the domain record actions.
@@ -40,12 +41,12 @@ class DomainActionsTest extends DomainTestBase {
     // Test some text on the page.
     foreach ($domains as $domain) {
       $name = $domain->label();
-      $this->assertText($name, format_string('@name found on overview page.', array('@name' => $name)));
+      $this->assertText($name, new FormattableMarkup('@name found on overview page.', array('@name' => $name)));
     }
     // @TODO: Test the list of actions.
     $actions = array('delete', 'disable', 'default');
     foreach ($actions as $action) {
-      $this->assertRaw("/domain/{$action}/", format_string('@action action found.', array('@action' => $action)));
+      $this->assertRaw("/domain/{$action}/", new FormattableMarkup('@action action found.', array('@action' => $action)));
     }
     // @TODO: Disable a domain and test the enable link.
 

domain/src/Tests/DomainCreateTest.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Drupal\domain\Tests;
+use Drupal\Component\Render\FormattableMarkup;
 
 /**
  * Tests the domain record creation API.
@@ -20,7 +21,7 @@ class DomainCreateTest extends DomainTestBase {
     $domain = \Drupal::service('domain.creator')->createDomain();
     foreach (array('id', 'name', 'hostname', 'domain_id', 'scheme', 'status', 'weight' , 'is_default') as $key) {
       $property = $domain->get($key);
-      $this->assertTrue(isset($property), format_string('New $domain->@key property is set to default value: %value.', array('@key' => $key, '%value' => $property)));
+      $this->assertTrue(isset($property), new FormattableMarkup('New $domain->@key property is set to default value: %value.', array('@key' => $key, '%value' => $property)));
     }
     $domain->save();
 

domain/src/Tests/DomainEntityReferenceTest.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Drupal\domain\Tests;
+use Drupal\Component\Render\FormattableMarkup;
 
 /**
  * Tests the domain record entity reference field type.
@@ -88,7 +89,7 @@ class DomainEntityReferenceTest extends DomainTestBase {
     $domains = \Drupal::service('domain.loader')->loadMultiple();
     foreach ($domains as $domain) {
       $string = 'value="' . $domain->id() . '"';
-      $this->assertRaw($string, format_string('Found the %domain option.', array('%domain' => $domain->label())));
+      $this->assertRaw($string, new FormattableMarkup('Found the %domain option.', array('%domain' => $domain->label())));
       if (!isset($one)) {
         $one = $domain->id();
         continue;

commit d42c2264be480fa1a3284b93cbb3afed9758381e

Fix Domain condition crashes blocks regression
Regression introduced in 1ff4051.

domain/src/Plugin/Condition/Domain.php
@@ -65,7 +65,7 @@ class Domain extends ConditionPluginBase implements ContainerFactoryPluginInterf
       '#type' => 'checkboxes',
       '#title' => $this->t('When the following domains are active'),
       '#default_value' => $this->configuration['domains'],
-      '#options' => array_map('\Drupal\Component\Utility\Html::checkPlain', \Drupal::service('domain.loader')->loadOptionsList()),
+      '#options' => array_map('\Drupal\Component\Utility\Html::escape', \Drupal::service('domain.loader')->loadOptionsList()),
       '#description' => $this->t('If you select no domains, the condition will evaluate to TRUE for all requests.'),
       '#attached' => array(
         'library' => array(