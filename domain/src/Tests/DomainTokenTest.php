8a807ecf8540bd7cd9021e122f12333ed31506a3
agentrickard <agentrickard@gmail.com>
2016-09-10 11:35:53 -0400

    Adds test file for tokens.

---
+++ domain/src/Tests/DomainTokenTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\domain\Tests;
+
+use Drupal\Core\Session\AccountInterface;
+use Drupal\domain\DomainInterface;
+
+/**
+ * Tests the domain token handler.
+ *
+ * @group domain
+ */
+class DomainTokenTest extends DomainTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = array('domain', 'block');
+
+  /**
+   * Tests the handling of an inbound request.
+   */
+  public function testDomainTokens() {
+    // No domains should exist.
+    $this->domainTableIsEmpty();
+
+    // Create four new domains programmatically.
+    $this->domainCreateTestDomains(4);
+
+    // Since we cannot read the service request, we place a block
+    // which shows the current domain information.
+    $this->drupalPlaceBlock('domain_server_block');
+
+    // To get around block access, let the anon user view the block.
+    user_role_grant_permissions(AccountInterface::ANONYMOUS_ROLE, array('view domain information'));
+
+    // Test the response of the default home page.
+    foreach (\Drupal::service('domain.loader')->loadMultiple() as $domain) {
+      $this->drupalGet($domain->getPath());
+      $this->assertRaw($domain->label(), 'Loaded the proper domain.');
+    }
+  }
+
+}
