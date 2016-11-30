9713eef8b8db54d69043a7162e640abb7af87f02
agentrickard <agentrickard@gmail.com>
2016-11-30 16:35:58 -0500

    Use dependency injection on the DomainListBuilder.

--- domain/src/DomainListBuilder.php
+++ domain/src/DomainListBuilder.php
@@ -4,9 +4,17 @@ namespace Drupal\domain;
 
 use Drupal\Core\Config\Entity\DraggableListBuilder;
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Routing\RedirectDestinationInterface;
+use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Url;
 use Drupal\domain\DomainAccessControlHandler;
+use Drupal\domain\DomainLoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * User interface for the domain overview screen.
@@ -17,6 +25,94 @@ class DomainListBuilder extends DraggableListBuilder {
    * {@inheritdoc}
    */
   protected $entitiesKey = 'domains';
+
+  /**
+   * The current user object.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $currentUser;
+
+  /**
+   * The redirect destination helper.
+   *
+   * @var \Drupal\Core\Routing\RedirectDestinationInterface
+   */
+  protected $destinationHandler;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The domain entity access control handler.
+   *
+   * @var \Drupal\domain\DomainAccessControlHandler
+   */
+  protected $accessHandler;
+
+  /**
+   * The module handler.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * The domain loader.
+   *
+   * @var \Drupal\domain\DomainLoaderInterface
+   */
+  protected $domainLoader;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function createInstance(ContainerInterface $container, EntityTypeInterface $entity_type) {
+    return new static(
+      $entity_type,
+      $container->get('entity.manager')->getStorage($entity_type->id()),
+      $container->get('current_user'),
+      $container->get('redirect.destination'),
+      $container->get('entity_type.manager'),
+      $container->get('module_handler'),
+      $container->get('domain.loader')
+    );
+  }
+
+  /**
+   * Constructs a new EntityListBuilder object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
+   *   The entity type definition.
+   * @param \Drupal\Core\Entity\EntityStorageInterface $storage
+   *   The entity storage class.
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   The active user account.
+   * @param \Drupal\Core\Routing\RedirectDestinationInterface $destination
+   *   The redirect destination helper.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The module handler.
+   * @param \Drupal\domain\DomainLoaderInterface $domain_loader
+   *   The domain loader.
+   */
+  public function __construct(EntityTypeInterface $entity_type, EntityStorageInterface $storage, AccountInterface $account, RedirectDestinationInterface $destination_handler, EntityTypeManagerInterface $entity_type_manager, ModuleHandlerInterface $module_handler, DomainLoaderInterface $domain_loader) {
+    parent::__construct($entity_type, $storage);
+    $this->entityTypeId = $entity_type->id();
+    $this->storage = $storage;
+    $this->entityType = $entity_type;
+    $this->currentUser = $account;
+    $this->destinationHandler = $destination_handler;
+    $this->entityTypeManager = $entity_type_manager;
+    $this->accessHandler = $this->entityTypeManager->getAccessControlHandler('domain');
+    $this->moduleHandler = $module_handler;
+    $this->domainLoader = $domain_loader;
+  }
 
   /**
    * {@inheritdoc}
@@ -30,20 +126,18 @@ class DomainListBuilder extends DraggableListBuilder {
    */
   public function getOperations(EntityInterface $entity) {
     $operations = parent::getOperations($entity);
-    $destination = \Drupal::destination()->getAsArray();
+    $destination = $this->destinationHandler->getAsArray();
     $default = $entity->isDefault();
     $id = $entity->id();
 
-    $accessHandler = \Drupal::entityTypeManager()->getAccessControlHandler('domain');
-
     // If the user cannot edit domains, none of these actions are permitted.
-    $admin = $accessHandler->checkAccess($entity, 'update');
+    $admin = $this->accessHandler->checkAccess($entity, 'update');
     if ($admin->isForbidden()) {
       return $operations;
     }
-    $account = \Drupal::currentUser();
-    $super_admin = $account->hasPermission('administer domains');
-    if ($account->hasPermission('access inactive domains')) {
+
+    $super_admin = $this->currentUser->hasPermission('administer domains');
+    if ($this->currentUser->hasPermission('access inactive domains')) {
       if ($entity->status() && !$default) {
         $operations['disable'] = array(
           'title' => $this->t('Disable'),
@@ -66,22 +160,21 @@ class DomainListBuilder extends DraggableListBuilder {
         'weight' => 30,
       );
     }
-    if (!$default && $accessHandler->checkAccess($entity, 'delete')->isAllowed()) {
+    if (!$default && $this->accessHandler->checkAccess($entity, 'delete')->isAllowed()) {
       $operations['delete'] = array(
         'title' => $this->t('Delete'),
         'url' => Url::fromRoute('entity.domain.delete_form', array('domain' => $id)),
         'weight' => 20,
       );
     }
-    // @TODO: inject this service?
-    $operations += \Drupal::moduleHandler()->invokeAll('domain_operations', array($entity));
+    $operations += $this->moduleHandler->invokeAll('domain_operations', array($entity));
     foreach ($operations as $key => $value) {
       if (isset($value['query']['token'])) {
         $operations[$key]['query'] += $destination;
       }
     }
     /** @var DomainInterface $default */
-    $default = \Drupal::service('domain.loader')->loadDefaultDomain();
+    $default = $this->domainLoader->loadDefaultDomain();
 
     // Deleting the site default domain is not allowed.
     if ($id == $default->id()) {
@@ -99,8 +192,7 @@ class DomainListBuilder extends DraggableListBuilder {
     $header['status'] = $this->t('Status');
     $header['is_default'] = $this->t('Default');
     $header += parent::buildHeader();
-    $account = \Drupal::currentUser();
-    if (!$account->hasPermission('administer domains')) {
+    if (!$this->currentUser->hasPermission('administer domains')) {
       unset($header['weight']);
     }
     return $header;
@@ -110,10 +202,8 @@ class DomainListBuilder extends DraggableListBuilder {
    * {@inheritdoc}
    */
   public function buildRow(EntityInterface $entity) {
-    $accessHandler = \Drupal::entityTypeManager()->getAccessControlHandler('domain');
-
     // If the user cannot view the domain, none of these actions are permitted.
-    $admin = $accessHandler->checkAccess($entity, 'view');
+    $admin = $this->accessHandler->checkAccess($entity, 'view');
     if ($admin->isForbidden()) {
       return;
     }
@@ -127,12 +217,12 @@ class DomainListBuilder extends DraggableListBuilder {
     $row['status'] = array('#markup' => $entity->status() ? $this->t('Active') : $this->t('Inactive'));
     $row['is_default'] = array('#markup' => ($entity->isDefault() ? $this->t('Yes') : $this->t('No')));
     $row += parent::buildRow($entity);
-    $account = \Drupal::currentUser();
-    if (!$account->hasPermission('administer domains')) {
+
+    if (!$this->currentUser->hasPermission('administer domains')) {
       unset($row['weight']);
     }
     else {
-      $row['weight']['#delta'] = count(\Drupal::service('domain.loader')->loadMultiple()) + 1;
+      $row['weight']['#delta'] = count($this->domainLoader->loadMultiple()) + 1;
     }
     return $row;
   }
@@ -145,8 +235,7 @@ class DomainListBuilder extends DraggableListBuilder {
     $form[$this->entitiesKey]['#domains'] = $this->entities;
     $form['actions']['submit']['#value'] = $this->t('Save configuration');
     // Only super-admins may sort domains.
-    $account = \Drupal::currentUser();
-    if (!$account->hasPermission('administer domains')) {
+    if (!$this->currentUser->hasPermission('administer domains')) {
       $form['actions']['submit']['#access'] = FALSE;
       unset($form['#tabledrag']);
     }
@@ -158,7 +247,6 @@ class DomainListBuilder extends DraggableListBuilder {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
-
     drupal_set_message($this->t('Configuration saved.'));
   }
 
