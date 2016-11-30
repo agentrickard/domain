3d612b7b026ddacd645fd609d5235a67d6a408b1
agentrickard <agentrickard@gmail.com>
2016-11-30 16:01:47 -0500

    Use the dependency injection pattern for the DomainAccessControlHandler.

--- domain/src/DomainAccessControlHandler.php
+++ domain/src/DomainAccessControlHandler.php
@@ -4,15 +4,67 @@ namespace Drupal\domain;
 
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Entity\EntityAccessControlHandler;
+use Drupal\Core\Entity\EntityHandlerInterface;
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\domain\DomainElementManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Defines the access controller for the domain entity type.
  *
  * Note that this is not a node access check.
  */
-class DomainAccessControlHandler extends EntityAccessControlHandler {
+class DomainAccessControlHandler extends EntityAccessControlHandler implements EntityHandlerInterface {
+
+ /**
+  * The entity type manager
+  *
+  * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+  */
+  protected $entityTypeManager;
+
+  /**
+   * The domain field element manager.
+   *
+   * @var \Drupal\domain\DomainElementManagerInterface
+   */
+  protected $domainElementManager;
+
+  /**
+   * The user storage manager.
+   */
+  protected $userStorage;
+
+  /**
+   * Constructs an access control handler instance.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
+   *   The entity type definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\domain\DomainElementManagerInterface $domain_element_manager
+   *   The domain field element manager.
+   */
+  public function __construct(EntityTypeInterface $entity_type, EntityTypeManagerInterface $entity_type_manager, DomainElementManagerInterface $domain_element_manager) {
+    parent::__construct($entity_type);
+    $this->entityTypeManager = $entity_type_manager;
+    $this->domainElementManager = $domain_element_manager;
+    $this->userStorage = $this->entityTypeManager->getStorage('user');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function createInstance(ContainerInterface $container, EntityTypeInterface $entity_type) {
+    return new static(
+      $entity_type,
+      $container->get('entity_type.manager'),
+      $container->get('domain.element_manager')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -53,7 +105,7 @@ class DomainAccessControlHandler extends EntityAccessControlHandler {
    * @return boolean
    */
   public function isDomainAdmin(EntityInterface $entity, AccountInterface $account) {
-    $user = \Drupal::entityTypeManager()->getStorage('user')->load($account->id());
+    $user = $this->userStorage->load($account->id());
     $user_domains = $this->getAdminValues($user);
     return isset($user_domains[$entity->id()]);
   }
@@ -72,7 +124,7 @@ class DomainAccessControlHandler extends EntityAccessControlHandler {
    * @TODO: Refactor this code so that it can be shared by other modules.
    */
   public function getAdminValues(EntityInterface $entity, $field_name = DOMAIN_ADMIN_FIELD) {
-    return \Drupal::service('domain.element_manager')->getFieldValues($entity, $field_name);
+    return $this->domainElementManager->getFieldValues($entity, $field_name);
   }
 
 }
