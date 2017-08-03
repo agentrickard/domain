91b66146363ab5ed09cf1284d79f966b7543ed97
azovsky <azovsky@330533.no-reply.drupal.org>
2017-08-03 11:59:31 -0400

    Issue #2869730 by azovsky: Drupal.settings.domain is undefined (domain.node.js)

--- domain.node.js
+++ domain.node.js
@@ -13,24 +13,27 @@ Drupal.behaviors.domainNodeFieldsetSummaries = {
   attach: function (context) {
     $('fieldset.domain-access-options-form').drupalSetSummary(function (context) {
 
-      var fieldTypeSelector = (Drupal.settings.domain.fieldType == 0) ? ':checked' : ':selected';
-      var allAffiliatesCheckbox = $('.form-item-domain-site input', context);
-      var domainInput = (fieldTypeSelector == ':checked') ? $('.form-item-domains input', context) : $('.form-item-domains select option', context);
-      domainInput = domainInput.filter(fieldTypeSelector);
-      var selectedDomainCount = domainInput.length;
+      if (typeof Drupal.settings.domain !== 'undefined') {
+        var fieldTypeSelector = (Drupal.settings.domain.fieldType == 0) ? ':checked' : ':selected';
+        var allAffiliatesCheckbox = $('.form-item-domain-site input', context);
+        var domainInput = (fieldTypeSelector == ':checked') ? $('.form-item-domains input', context) : $('.form-item-domains select option', context);
+        domainInput = domainInput.filter(fieldTypeSelector);
+        var selectedDomainCount = domainInput.length;
 
-      if (allAffiliatesCheckbox.is(':checked')) {
-        return Drupal.t('All affiliates');
+        if (allAffiliatesCheckbox.is(':checked')) {
+          return Drupal.t('All affiliates');
+        }
+        else if (selectedDomainCount == 0) {
+          return Drupal.t('No affiliates');
+        }
+        else if (selectedDomainCount == 1) {
+          return (fieldTypeSelector == ':checked') ? domainInput.siblings('label').first().text() : domainInput.first().text();
+        }
+        else {
+          return Drupal.t('@count affiliates', {'@count': selectedDomainCount});
+        }
       }
-      else if (selectedDomainCount == 0) {
-        return Drupal.t('No affiliates');
-      }
-      else if (selectedDomainCount == 1) {
-        return (fieldTypeSelector == ':checked') ? domainInput.siblings('label').first().text() : domainInput.first().text();
-      }
-      else {
-        return Drupal.t('@count affiliates', {'@count': selectedDomainCount});
-      }
+
     });
   }
 };
