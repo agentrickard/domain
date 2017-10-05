01a4eee277a2167e5b0e0e4d678fee87313c1286
Ken Rickard <agentrickard@gmail.com>
2017-10-05 10:12:48 -0400

    Add work by @mnugter on middleware tests.

--- domain_config/tests/modules/domain_config_middleware_test/src/Middleware.php
+++ domain_config/tests/modules/domain_config_middleware_test/src/Middleware.php
@@ -2,10 +2,8 @@
 
 namespace Drupal\domain_config_middleware_test;
 
-use Drupal\Component\Utility\Crypt;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 /**
