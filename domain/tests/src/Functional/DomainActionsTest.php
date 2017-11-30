fae48703203869acbdb4870805447eecbfce1edf
Ken Rickard <agentrickard@gmail.com>
2017-11-30 15:44:15 -0500

    Proper tests for Domain Actions.

--- domain/tests/src/Functional/DomainActionsTest.php
+++ domain/tests/src/Functional/DomainActionsTest.php
@@ -29,7 +29,7 @@ class DomainActionsTest extends DomainTestBase {
 
     // Test the domains.
     $storage = \Drupal::service('entity_type.manager')->getStorage('domain');
-    $domains = $storage->loadMultiple(NULL, TRUE);
+    $domains = $storage->loadMultiple();
     $this->assertTrue(count($domains) == 4, 'Four domain records found.');
 
     // Check the default domain.
@@ -42,16 +42,60 @@ class DomainActionsTest extends DomainTestBase {
       $name = $domain->label();
       $this->assertText($name, 'Name found properly.');
     }
-    // @TODO: Test the list of actions.
+    // Test the list of actions.
     $actions = array('delete', 'disable', 'default');
     foreach ($actions as $action) {
       $this->assertRaw("/domain/{$action}/", 'Actions found properly.');
     }
-    // @TODO: Disable a domain and test the enable link.
+    // Check that all domains are active.
+    $this->assertNoRaw('Inactive', 'Inactive domain not found.');
 
-    // @TODO: test the link behaviors.
+    // Disable a domain and test the enable link.
+    $this->clickLink('Disable', 0);
+    $this->assertRaw('Inactive', 'Inactive domain found.');
 
-    // @TODO: test permissions on actions
+    // Visit the domain overview administration page to clear cache.
+    $this->drupalGet($path);
+    $this->assertResponse(200);
+
+    foreach ($storage->loadMultiple() as $domain) {
+      if ($domain->id() == 'one_example_com') {
+        $this->assertEmpty($domain->status(), 'One domain inactive.');
+      }
+      else {
+        $this->assertNotEmpty($domain->status(), 'Other domains active.');
+      }
+    }
+
+    // Test the list of actions.
+    $actions = array('enable', 'delete', 'disable', 'default');
+    foreach ($actions as $action) {
+      $this->assertRaw("/domain/{$action}/", 'Actions found properly.');
+    }
+    // Re-enable the domain.
+    $this->clickLink('Enable', 0);
+    $this->assertNoRaw('Inactive', 'Inactive domain not found.');
+
+    // Visit the domain overview administration page to clear cache.
+    $this->drupalGet($path);
+    $this->assertResponse(200);
+
+    foreach ($storage->loadMultiple() as $domain) {
+      $this->assertNotEmpty($domain->status(), 'All domains active.');
+    }
+
+    // Set a new default domain.
+    $this->clickLink('Make default', 0);
+
+    // Visit the domain overview administration page to clear cache.
+    $this->drupalGet($path);
+    $this->assertResponse(200);
+
+    // Check the default domain.
+    $storage->resetCache();
+    $default = $storage->loadDefaultId();
+    $key = 'one_example_com';
+    $this->assertTrue($default == $key, 'Default domain set correctly.');
 
   }
 
