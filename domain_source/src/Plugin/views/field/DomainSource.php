f642893bc1ee8a68040e1d6d5a04bba50e1db6de
Ken Rickard <agentrickard@gmail.com>
2017-12-04 11:33:12 -0500

    Adds the new Domain Source views handlers.

---
+++ domain_source/src/Plugin/views/field/DomainSource.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\domain_source\Plugin\views\field;
+
+use Drupal\views\Plugin\views\field\EntityField;
+use Drupal\views\ResultRow;
+
+/**
+ * Field handler to present the link an entity on a domain.
+ *
+ * @ViewsField("domain_source")
+ */
+class DomainSource extends EntityField {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getItems(ResultRow $values) {
+    $items = parent::getItems($values);
+    // Override the default link generator, which wants to send us to the entity
+    // page, not the entity we are looking at.
+    if (!empty($this->options['settings']['link'])) {
+      foreach ($items as &$item) {
+        $object = $item['raw'];
+        $entity = $object->getEntity();
+        $url = $entity->toUrl()->toString();
+        $domain = $item['rendered']['#options']['entity'];
+        $item['rendered']['#type'] = 'markup';
+        $item['rendered']['#markup'] = '<a href="' . $domain->buildUrl($url) . '">' . $domain->label() . '</a>';
+      }
+      uasort($items, array($this, 'sort'));
+    }
+
+    return $items;
+  }
+
+  /**
+   * Sort the domain list, if possible.
+   */
+  private function sort($a, $b) {
+    $domainA = isset($a['rendered']['#options']['entity']) ? $a['rendered']['#options']['entity'] : 0;
+    $domainB = isset($b['rendered']['#options']['entity']) ? $b['rendered']['#options']['entity'] : 0;
+    if ($domainA !== 0) {
+      return $domainA->getWeight() > $domainB->getWeight();
+    }
+    // We don't have a domain object so sort as best we can.
+    return $a['rendered']['#plain_text'] > $b['rendered']['#plain_text'];
+  }
+
+}
---
+++ domain_source/src/Plugin/views/filter/DomainSource.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Drupal\domain_source\Plugin\views\filter;
+
+use Drupal\views\Plugin\views\filter\InOperator;
+
+/**
+ * Filter by published status.
+ *
+ * @ingroup views_filter_handlers
+ *
+ * @ViewsFilter("domain_source")
+ */
+class DomainSource extends InOperator {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getValueOptions() {
+    if (!isset($this->valueOptions)) {
+      $this->valueTitle = $this->t('Domains');
+      $this->valueOptions = [
+        '_active' => $this->t('Active domain'),
+      ] + \Drupal::service('entity_type.manager')->getStorage('domain')->loadOptionsList();
+    }
+    return $this->valueOptions;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    $active_index = array_search('_active', (array)$this->value);
+    if ($active_index !== FALSE) {
+      $active_id = \Drupal::service('domain.negotiator')->getActiveId();
+      $this->value[$active_index] = $active_id;
+    }
+
+    parent::query();
+  }
+}
