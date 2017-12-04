197bf4b5f3ed8e820c6bb2dfee1b6d7164e9572f
Ken Rickard <agentrickard@gmail.com>
2017-12-04 14:49:18 -0500

    Adds configuration options to the navigation block.

--- domain/src/Plugin/Block/DomainNavBlock.php
+++ domain/src/Plugin/Block/DomainNavBlock.php
@@ -3,6 +3,7 @@
 namespace Drupal\domain\Plugin\Block;
 
 use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Session\AccountInterface;
 
 /**
@@ -16,6 +17,45 @@ use Drupal\Core\Session\AccountInterface;
 class DomainNavBlock extends DomainBlockBase {
 
   /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $elements['link_options'] = [
+      '#title' => $this->t('Link paths'),
+      '#type' => 'radios',
+      '#required' => TRUE,
+      '#options' => ['active' => $this->t('Link to active url'), 'home' => $this->t('Link to site home page')],
+      '#default_value' => !empty($this->configuration['link_options']) ? $this->configuration['link_options'] : 'active',
+      '#description' => $this->t('Determines how links to each domain will be written. Note that some paths may not be accessible on all domains.'),
+    ];
+    $options = array(
+      'default' => t('JavaScript select list'),
+      'menus' => t('Menu-style tab links'),
+      'ul' => t('Unordered list of links'),
+    );
+    $elements['link_theme'] = array(
+      '#type' => 'radios',
+      '#title' => t('Link theme'),
+      '#default_value' => !empty($this->configuration['link_theme']) ? $this->configuration['link_theme'] : 'default',
+      '#options' => $options,
+      '#description' => $this->t('Select how to display the block output.'),
+    );
+    return $elements;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    // Process the block's submission handling if no errors occurred only.
+    if (!$form_state->getErrors()) {
+      foreach (['link_options', 'link_theme'] as $element) {
+        $this->configuration[$element] = $form_state->getValue($element);
+      }
+    }
+  }
+
+  /**
    * Overrides \Drupal\block\BlockBase::access().
    */
   public function access(AccountInterface $account, $return_as_object = FALSE) {
@@ -25,18 +65,18 @@ class DomainNavBlock extends DomainBlockBase {
 
   /**
    * Build the output.
-   *
-   * @TODO: abstract or theme this function?
    */
   public function build() {
     /** @var \Drupal\domain\DomainInterface $active_domain */
     $active_domain = \Drupal::service('domain.negotiator')->getActiveDomain();
-    $items = [];
     $access_handler = \Drupal::service('entity_type.manager')->getAccessControlHandler('domain');
     $account = \Drupal::currentUser();
+    $current_path = TRUE;
+    $items = [];
     /** @var \Drupal\domain\DomainInterface $domain */
     foreach (\Drupal::service('entity_type.manager')->getStorage('domain')->loadMultipleSorted() as $domain) {
-      $string = $domain->getLink();
+      $current_path = ($this->configuration['link_options'] == 'active');
+      $string = $domain->getLink($current_path);
       if ($domain->access('view', $account)) {
         $items[] = array('#markup' => $string);
       }
