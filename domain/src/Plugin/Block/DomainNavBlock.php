b3c342f7c422ae230587faad3b56d14423819282
Ken Rickard <agentrickard@gmail.com>
2017-12-04 16:38:53 -0500

    Adds menu theme option and configurable label text.

--- domain/src/Plugin/Block/DomainNavBlock.php
+++ domain/src/Plugin/Block/DomainNavBlock.php
@@ -5,6 +5,8 @@ namespace Drupal\domain\Plugin\Block;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
 
 /**
  * Provides a block that links to all active domains.
@@ -30,7 +32,7 @@ class DomainNavBlock extends DomainBlockBase {
       '#description' => $this->t('Determines how links to each domain will be written. Note that some paths may not be accessible on all domains.'),
     ];
     $options = array(
-      'default' => t('JavaScript select list'),
+      'select' => t('JavaScript select list'),
       'menus' => t('Menu-style tab links'),
       'ul' => t('Unordered list of links'),
     );
@@ -41,6 +43,18 @@ class DomainNavBlock extends DomainBlockBase {
       '#options' => $options,
       '#description' => $this->t('Select how to display the block output.'),
     );
+    $options = array(
+      'name' => t('The domain display name'),
+      'hostname' => t('The raw hostname'),
+      'url' => t('The domain base URL'),
+    );
+    $elements['link_label'] = array(
+      '#type' => 'radios',
+      '#title' => t('Link text'),
+      '#default_value' => !empty($this->configuration['link_label']) ? $this->configuration['link_label'] : $defaults['link_label'],
+      '#options' => $options,
+      '#description' => $this->t('Select the text to display for each link.'),
+    );
     return $elements;
   }
 
@@ -50,7 +64,7 @@ class DomainNavBlock extends DomainBlockBase {
   public function blockSubmit($form, FormStateInterface $form_state) {
     // Process the block's submission handling if no errors occurred only.
     if (!$form_state->getErrors()) {
-      foreach (['link_options', 'link_theme'] as $element) {
+      foreach (array_keys($this->defaultConfiguration()) as $element) {
         $this->configuration[$element] = $form_state->getValue($element);
       }
     }
@@ -76,7 +90,7 @@ class DomainNavBlock extends DomainBlockBase {
     // Get the settings.
     $settings = [];
     $defaults = $this->defaultConfiguration();
-    foreach (['link_options', 'link_theme'] as $element) {
+    foreach (array_keys($this->defaultConfiguration()) as $element) {
       if (isset($this->configuration[$element])) {
         $settings[$element] = $this->configuration[$element];
       }
@@ -87,17 +101,62 @@ class DomainNavBlock extends DomainBlockBase {
 
     // Determine the visible domain list.
     $items = [];
+    $add_path = ($settings['link_options'] == 'active');
     /** @var \Drupal\domain\DomainInterface $domain */
     foreach (\Drupal::service('entity_type.manager')->getStorage('domain')->loadMultipleSorted() as $domain) {
-      $current_path = ($settings['link_options'] == 'active');
-      $string = $domain->getLink($current_path);
+      // Set the URL.
+      $options = ['absolute' => TRUE, 'https' => ($domain->getScheme() == 'https')];
+      if ($add_path) {
+        $url = Url::fromUri($domain->getUrl(), $options);
+      }
+      else {
+        $url = Url::fromUri($domain->getPath(), $options);
+      }
+      // Set the label text.
+      $label = $domain->label();
+      if ($settings['link_label'] == 'hostname') {
+        $label = $domain->getHostname();
+      }
+      elseif ($settings['link_label'] == 'url') {
+        $label = $domain->getPath();
+      }
+      // Handles menu links.
       if ($domain->status() || $account->hasPermission('access inactive domains')) {
-        $items[] = array('#markup' => $string);
+        if ($settings['link_theme'] == 'menus') {
+          // @TODO: active trail isn't working properly, likely because this
+          // isn't really a menu.
+          $items[] = [
+            'title' => $label,
+            'url' => $url,
+            'attributes' => $domain->isActive() ? ['classes' => ['menu-item--active-trail']] : [],
+            'below' => [],
+            'is_expanded' => FALSE,
+            'is_collapsed' => FALSE,
+            'in_active_trail' => FALSE,
+          ];
+        }
+        else {
+          $items[] = array('#markup' => Link::fromTextAndUrl($label, $url)->toString());
+        }
       }
     }
 
+    // Set the proper theme.
+    switch ($settings['link_theme']) {
+      case 'select':
+        $theme = 'new_theme';
+        break;
+      case 'menus':
+        $theme = 'menu';
+        break;
+      case 'ul':
+      default:
+        $theme = 'item_list';
+        break;
+    }
+
     return array(
-      '#theme' => 'item_list',
+      '#theme' => $theme,
       '#items' => $items,
     );
   }
@@ -108,7 +167,8 @@ class DomainNavBlock extends DomainBlockBase {
   public function defaultConfiguration() {
     return [
       'link_options' => 'home',
-      'link_theme' => 'default',
+      'link_theme' => 'ul',
+      'link_label' => 'name',
     ];
   }
 
