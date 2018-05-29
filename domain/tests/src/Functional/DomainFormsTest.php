cd1160ec3f09a91fefbcff865103bff7dd38b608
Ken Rickard <agentrickard@gmail.com>
2018-05-29 15:44:39 -0400

    Fixes travis fails; expands testing

--- domain/tests/src/Functional/DomainFormsTest.php
+++ domain/tests/src/Functional/DomainFormsTest.php
@@ -27,7 +27,12 @@ class DomainFormsTest extends DomainTestBase {
     $this->drupalGet('admin/config/domain');
 
     // Check for the add message.
-    $this->assertText('There are no domain record entities yet.', 'Text for no domains found.');
+    if (substr_count(Drupal::VERSION, '8.5') > 0) {
+      $this->assertText('There is no Domain record yet.', 'Text for no domains found.');
+    }
+    else {
+      $this->assertText('There are no domain record entities yet.', 'Text for no domains found.');
+    }
     // Visit the add domain administration page.
     $this->drupalGet('admin/config/domain/add');
 
