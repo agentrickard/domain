2e8db2ff104b43fa36363cf6f0b597ce02a41e39
Gordon Heydon <gordon@heydon.com.au>
2019-03-05 15:20:46 +1100

    Create some handy twig global variables and functions to assist with theming.

---
+++ domain/src/TwigExtension.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\domain;
+
+/**
+ * Class TwigExtension
+ *
+ * @package Drupal\domain
+ */
+class TwigExtension extends \Twig_Extension {
+
+  /**
+   * @var \Drupal\domain\DomainNegotiatorInterface
+   */
+  protected $domainNegotiator;
+
+  /**
+   * TwigExtension constructor.
+   *
+   * @param \Drupal\domain\DomainNegotiatorInterface $domainNegotiator
+   *   Domain negotiator to work out the current domain.
+   */
+  public function __construct(DomainNegotiatorInterface $domainNegotiator) {
+    $this->domainNegotiator = $domainNegotiator;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getGlobals() {
+    return [
+      'domain_active' => $this->domainNegotiator->getActiveDomain(),
+      'domain_active_id' => $this->domainNegotiator->getActiveId(),
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFunctions() {
+    new \Twig_SimpleFunction('is_domain', [$this, 'isDomain']);
+  }
+
+  /**
+   * Check of the current domain id.
+   *
+   * @param $domain_id
+   *   Id of the domain to check.
+   *
+   * @return bool
+   *   if the id passed in a the current domain.
+   */
+  public function isDomain($domain_id) {
+    return $this->domainNegotiator->getActiveId() == $domain_id;
+  }
+}
