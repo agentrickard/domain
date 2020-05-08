b593fb420943149eea3368c7d89c576b328ac024
agentrickard <agentrickard@gmail.com>
2020-05-08 10:28:03 -0400

    Fix for drupal.org testing from https://www.drupal.org/project/infrastructure/issues/3133850.

--- define_subdomains.sh
+++ define_subdomains.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+set -uex
 
 [[ ${PROJECT_DIR} =~ workspace\/(.*?)\/source ]] && echo "${BASH_REMATCH[1]}"
 CONTAINER_NAME=${BASH_REMATCH[1]}
 CONTAINER_NAME=${CONTAINER_NAME//_/-}
 CONTAINER_NAME='php-apache-'$CONTAINER_NAME
 
-declare -a HOSTS=(${CONTAINER_NAME} 'example.com')
+declare -a HOSTS=(${CONTAINER_NAME} 'example.local')
 declare -a SUBDOMAINS=('one' 'two' 'three' 'four' 'five' 'six' 'seven' 'eight' 'nine' 'ten')
 
 for HOST in ${HOSTS[@]}; do
@@ -15,3 +16,5 @@ for HOST in ${HOSTS[@]}; do
       echo '127.0.0.1' ${SUBDOMAIN}.${HOST} >> /etc/hosts
     done
 done
+
+cat /etc/hosts
